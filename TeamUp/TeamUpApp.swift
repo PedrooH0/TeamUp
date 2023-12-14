758a3e94ebeb8e9e3c87e05051804dc657aee0de
Pedro Henrique <phfarias210120@gmail.com>
2023-12-14 15:02:15 -0300

    Initial Commit

---
+++ TeamUp/TeamUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamUpApp.swift
+//  TeamUp
+//
+//  Created by user on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TeamUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
